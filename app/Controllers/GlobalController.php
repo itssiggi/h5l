17d3b5b30585237f895244d4d55e63ce803ea211
themkde <grex1@gmx.de>
2020-08-16 11:44:00 +0200

    Simplified standing calc, based on scopes

--- app/Controllers/GlobalController.php
+++ app/Controllers/GlobalController.php
@@ -88,7 +88,8 @@ class GlobalController extends Controller
                         $results = $session->results;
                         if ($results) {
                             foreach ($results as $result) {
-                                $driver_ids[$result->driver_id] = 0;
+                                $driver_points[$result->driver_id] = 0;
+                                $driver_wins[$result->driver_id] = 0;
                             }
                         }
                     }
@@ -96,77 +97,36 @@ class GlobalController extends Controller
             }
 
             # Generate new Standings
-            foreach ($events as $event) { 
-                $sessions = Session::where('event_id', $event->id)->where('type', 10)->get();
-                if ($sessions) {
-                    foreach ($sessions as $session) {
-                        $results = $session->results;
-                        $temp_driver_ids = $driver_ids;
-
-                        if ($results) {
-                            foreach ($results as $result) {
-                                $last_standing = Standing::where('driver_id', $result->driver_id)->where('season_id', $season_id)->orderBy('id', 'DESC')->first();
-
-                                if ($last_standing) {
-                                    if ($result->position == 1) {
-                                        $wins = $last_standing->wins + 1;
-                                    } else {
-                                        $wins = $last_standing->wins;
-                                    }
-                                    $points = $last_standing->points + $result->points;
-
-                                    if ($last_standing->event_id == $event->id) {
-                                        $last_standing->forceDelete();
-                                    }
-                                } else {
-                                    $points = $result->points;
-                                    $wins = intval($result->position == 1);
-                                }
-                                $standing = new Standing;
-                                $standing->event_id = $event->id;
-                                $standing->season_id = $season_id;
-                                $standing->driver_id = $result->driver_id;
-                                $standing->points = $points;
-                                $standing->wins = $wins;
-                                $standing->save();
-                                $temp_driver_ids[$result->driver_id] = 1;
-                            }
-                        }
-                        foreach ($temp_driver_ids as $driver_id => $value) {
-                            if ($value == 0) {
-                                $last_standing = Standing::where('driver_id', $driver_id)->where('season_id', $season_id)->orderBy('id', 'DESC')->first();
-                                if ($last_standing) {
-                                    $standing = new Standing;
-                                    $standing->event_id = $event->id;
-                                    $standing->season_id = $event->season_id;
-                                    $standing->driver_id = $driver_id;
-                                    $standing->points = $last_standing->points;
-                                    $standing->wins = $last_standing->wins;
-                                    $standing->save();
-                                    if ($last_standing->event_id == $event->id) {
-                                        $last_standing->forceDelete();
-                                    }
-                                } else {
-                                    $standing = new Standing;
-                                    $standing->event_id = $event->id;
-                                    $standing->season_id = $event->season_id;
-                                    $standing->driver_id = $driver_id;
-                                    $standing->points = 0;
-                                    $standing->wins = 0;
-                                    $standing->save();
-                                }
-                            }
-                        }
-                        $standings = Standing::where('event_id', $event->id)->orderBy('points', 'DESC')->get();
-                        $position = 1;
-                        foreach ($standings as $standing) {
-                            $standing->position = $position;
-                            $position += 1;
-                            $standing->save();
+            foreach ($events as $event) {
+                foreach ($driver_points as $driver_id => $value) {
+                    $results = Result::fromDriver($driver_id)->fromEvent($event->id)->isOfficial()->isRace()->get();
+                    foreach ($results as $result) {
+                        $driver_ids[$driver_id] += $result->points;
+                        if ($result->position == 1) {
+                            $driver_wins[$driver_id] += 1;
                         }
                     }
+
+                    $standing = new Standing;
+                    $standing->event_id = $event->id;
+                    $standing->season_id = $season_id;
+                    $standing->driver_id = $driver_id;
+                    $standing->points = $driver_ids[$driver_id];
+                    $standing->wins = $driver_wins[$driver_id];
+                    $standing->save();
+                }
+
+                // Calculate Positions
+                $standings = Standing::fromEvent($event->id)->orderBy('points', 'DESC')->get();
+                $position = 1;
+                foreach ($standings as $standing) {
+                    $standing->position = $position;
+                    $position += 1;
+                    $standing->save();
                 }
             }
+
+
         }
     }
 }
